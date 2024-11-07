db14fb35bb74e31ef903f34024920630fdfb975c
hyosoek <tennfin1@gmail.com>
2024-11-07 14:31:31 +0900

    First commit

---
+++ src/test/java/com/example/compilejava/CompileJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.compilejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompileJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
